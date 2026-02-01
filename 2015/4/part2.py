e625a1540de4d85dd2ccdb6fb422c37cc1f74bb4
spac3y <73653851+Spac3y@users.noreply.github.com>
2026-02-01 16:15:48 +0200

    4 2015

---
+++ 2015/4/part2.py
@@ -0,0 +1,29 @@
+import time
+import hashlib
+
+INPUT = 'bgvyzdsv'
+
+append = 0
+
+def check(input):
+	res = hashlib.md5(bytes(input, 'utf-8'))
+	hex = res.hexdigest()
+	for i in range(0,6):
+		if hex[i].isdigit():
+			if int(hex[i]) != 0:
+				return False
+		else: return False
+	return True
+
+while True:
+	# stop = 610000
+	append += 1
+	
+	temp = INPUT + str(append)
+
+	print(f"{temp}")
+
+	if check(temp):
+		print(append)
+		break
+	# time.sleep(0.2)
